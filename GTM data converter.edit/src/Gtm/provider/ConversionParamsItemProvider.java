2ac81340b983ea07977562b3163984e2601ea56d
CGantert345 <57003061+CGantert345@users.noreply.github.com>
2020-06-03 16:39:03 +0200

    documentation of converter

--- GTM data converter.edit/src/Gtm/provider/ConversionParamsItemProvider.java
+++ GTM data converter.edit/src/Gtm/provider/ConversionParamsItemProvider.java
@@ -258,14 +258,18 @@ public class ConversionParamsItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
-		String label = ((ConversionParams)object).getTaxId();
+		return " ";
+		/*
+		 * String label = ((ConversionParams)object).getTaxId();
+
 		return label == null || label.length() == 0 ?
 			getString("_UI_ConversionParams_type") :
 			getString("_UI_ConversionParams_type") + " " + label;
+					 */
 	}
 
 
