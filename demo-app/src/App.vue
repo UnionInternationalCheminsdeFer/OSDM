ace3efbdd3b28b6e26d051ed1951ece9715099d6
Andreas Schlapbach <andreas.schlapbach@sbb.ch>
2024-12-08 13:19:28 +0100

    Verbesserung des Layouts der Demo-App: Titel aktualisiert und Fußzeile hinzugefügt; Farbänderungen in der CSS-Datei

--- demo-app/src/App.vue
+++ demo-app/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="h-full">
     <header class="bg-osdm-bg-primary text-osdm-text-inverted flex justify-center p-4 text-xl font-medium">
-      <span>OSDM-Demo-App</span>
+      <h1 class="self-center text-4xl m-3">OSDM Demo App</h1>
+
       <div class="absolute right-4 top-4">
         <SandboxSettings />
       </div>
     </header>
     <RouterView />
+    <footer class="bg-osdm-bg-primary text-sm text-osdm-text-inverted flex justify-center p-4">
+      <p>Kindly brought to you by <a href="http://xatelitte.space">xatelitte</a>.</p>
+    </footer>
   </div>
 </template>
 
