026cc24c3ed055780adc82fb1ccf06f85e2959b5
TheNewCivilian <git@prenninger.de>
2024-12-09 10:18:10 +0100

    Demo-App: remove use of proxy server

--- demo-app/src/auth.ts
+++ demo-app/src/auth.ts
@@ -1,19 +1,45 @@
 import type { Middleware } from 'openapi-fetch'
 import { useAuthStore } from './stores/auth'
 
-const getAccessToken = async () => {
+export class AccessToken {
+  token: string | null
+  tokenType: string | null
+  tokenExpiration: number | null
+
+  constructor() {
+    this.token = null
+    this.tokenType = null
+    this.tokenExpiration = null
+  }
+
+  async get() {
+    if (!this.token || !this.tokenExpiration || this.tokenExpiration > Date.now()) {
+      await this.refresh()
+    }
+    return `${this.tokenType} ${this.token}`
+  }
+
+  async refresh() {
     const response = await fetch(
-        `${useAuthStore().sandboxURL}/oauth/token?grant_type=client_credentials&client_id=${useAuthStore().clientId}&client_secret=${useAuthStore().clientSecret}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await response.json();
-      // ToDo: Add token expired check
-      return data.access_token;
+      `${useAuthStore().sandboxURL}/oauth/token?grant_type=client_credentials&client_id=${useAuthStore().clientId}&client_secret=${useAuthStore().clientSecret}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+
+    if (!data.access_token || !data.token_type || !data.expires_in) {
+      this.token = null
+      this.tokenType = null
+      this.tokenExpiration = null
+    }
+    this.token = data.access_token
+    this.tokenType = data.token_type
+    this.tokenExpiration = Date.now() + data.expires_in
+  }
 }
 
 export const AuthMiddleware: Middleware = {
@@ -22,16 +48,12 @@ export const AuthMiddleware: Middleware = {
     if (useAuthStore().sandboxURL.length <= 0) {
         return request
     }
-    let accessToken = useAuthStore().accessToken;
 
-    if (accessToken.length <= 0) {
-        accessToken = await getAccessToken();
-        useAuthStore().accessToken = accessToken;
-    }
+    const accessToken = await useAuthStore().accessToken.get();
 
     request.headers.set(
         'Authorization',
-        `Bearer ${accessToken}`
+        accessToken
     )
 
     return request
