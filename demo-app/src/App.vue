8f5ba41323f5af88ec1a969d5a51bbb5fb3d0f22
Andreas Schlapbach <andreas.schlapbach@sbb.ch>
2024-12-09 08:24:22 +0100

    Verbesserung des Layouts der Demo-App: Titel aktualisiert und Fußzeile hinzugefügt; Farbänderungen in der CSS-Datei

--- demo-app/src/App.vue
+++ demo-app/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="h-full">
-    <header class="bg-osdm-bg-primary text-osdm-text-inverted flex justify-center p-4 text-xl font-medium">
-      <h1 class="self-center text-4xl m-3">OSDM Demo App</h1>
-
+    <header class="bg-osdm-bg-primary flex justify-center p-2 pt-9 text-xl font-medium">
+      <h1 class="self-center text-4xl m-3 font-bold">OSDM Demo App</h1>
       <div class="absolute right-4 top-4">
         <SandboxSettings />
       </div>
