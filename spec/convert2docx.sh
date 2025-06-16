aa02d77a91d7337091ed81bf6fb9519264a1229a
CGantert345 <57003061+CGantert345@users.noreply.github.com>
2025-06-16 09:32:29 +0200

    Update convert2docx.sh

--- spec/convert2docx.sh
+++ spec/convert2docx.sh
@@ -3,23 +3,27 @@
 # Needs pandoc to be installed
 
 pandoc -s \
--o OSDM-specification-V3.2-rc.docx \
+-o OSDM-specification-V3.6-rc.docx \
 foreword.md \
-../releases/OSDM-release-notes-v3.2.md \
+../releases/OSDM-release-notes-v3.6.md \
 requirements.md \
 business-capabilities.md \
 common-data-structures.md \
 relationship-offer-offerpart-product-and-fare.md \
 technical-principles.md \
+authentication.md \
 offline-model.md \
 roles.md \
 models.md \
 processes.md \
+after-sales-processes.md \
+account-based-ticketing.md \
 getting-started.md \
 non-functional-requirements.md \
 catalog-of-code-lists.md \
-errors-warnings.md \
+errors-problems.md \
 pagination.md \
 synchronization.md \
 compliance.md \
+
 
